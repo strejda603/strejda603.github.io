683d3fbc7ad72d0e79cd7c4b9e0fb81a5718eb84
Strejda603 <strejda603@centrum.cz>
2019-08-14 14:21:07 +0200

    pkg update

--- description.js
+++ description.js
@@ -1,20 +1,3 @@
-function iOSversion() {
-	if (/iP(hone|od|ad)/.test(navigator.platform)) {
-		var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
-		return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
-	}
-	else {
-		return 0;
-	}
-}
-ver = iOSversion();
-if (ver[0] >= 11 || (ver[0] === 12 && ver[1] <= 2)) {
-	document.getElementById('ios').innerHTML='<p><strong>✓</strong> Your device is supported.</p>';
-}
-else {
-	document.getElementById('ios').innerHTML='<p><strong>x</strong> Your device is not supported.</p>';
-}
-
 function loadPackageInfo() {
 	var urlSplit = window.location.href.split('description.html?id=');
 	var formURL = urlSplit[0] + "packageInfo/" + urlSplit[1];
---
+++ iosver.js
@@ -0,0 +1,16 @@
+function iOSversion() {
+	if (/iP(hone|od|ad)/.test(navigator.platform)) {
+		var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
+		return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
+	}
+	else {
+		return 0;
+	}
+}
+ver = iOSversion();
+if (ver[0] >= 11 || (ver[0] === 12 && ver[1] <= 2)) {
+	document.getElementById('ios').innerHTML='<p><strong>✓</strong> Your device is supported.</p>';
+}
+else {
+	document.getElementById('ios').innerHTML='<p><strong>x</strong> Your device is not supported.</p>';
+}
