83cd1dc38139dceb8701f4f8dfb2994b94c38c5b
Strejda603 <strejda603@users.noreply.github.com>
2019-08-31 00:00:11 +0200

    pkg update

--- iosver.js
+++ iosver.js
@@ -1,3 +1,8 @@
+//Thank you Matchstic, you beautiful person
+const VERSION_CHECK_SUPPORTED = "<p>iOS %s is supported. ✓</p>";
+const VERSION_CHECK_UNSUPPORTED = "<p>iOS %s is not supported. ✕</p>";
+const VERSION_CHECK_UNCONFIRMED = "<p>iOS %s has not been tested!</p>";
+
 function iOSversion() {
 	if (/iP(hone|od|ad)/.test(navigator.platform)) {
 		var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
@@ -7,10 +12,22 @@ function iOSversion() {
 		return 0;
 	}
 }
-ver = iOSversion();
-if (ver[0] >= 11) {
-	document.getElementById('ios').innerHTML='<p>Your device is supported.</p>';
-}
-else {
-	document.getElementById('ios').innerHTML='<p>Your device is not supported.</p>';
-}
+
+var maxVersion = document.getElementById('maxVersion').innerHTML;
+var minVersion = document.getElementById('minVersion').innerHTML;
+var version = iOSversion();
+var versionNumber = parseFloat(version[0] + "." + version[1]);
+
+	if(versionNumber >= minVersion && version <= maxVersion) {
+		document.getElementById('compatibility').innerHTML=VERSION_CHECK_SUPPORTED.replace("%s", versionNumber);
+		document.body.style.setProperty("--title-color", "rgba(39,174,96, 0.5)");
+	} else if(versionNumber > maxVersion) {
+		document.getElementById('compatibility').innerHTML=VERSION_CHECK_UNCONFIRMED.replace("%s", versionNumber);
+		document.body.style.setProperty("--title-color", "rgba(192,57,43, 0.5)");
+	} else if(!versionNumber) {
+		document.getElementById('compatibility').innerHTML="<p>Can't get device version.</p>";
+		document.body.style.setProperty("--title-color", "rgba(192,57,43, 0.5)");
+	} else {
+		document.getElementById('compatibility').innerHTML=VERSION_CHECK_UNSUPPORTED.replace("%s", versionNumber);
+		document.body.style.setProperty("--title-color", "rgba(192,57,43, 0.5)");
+	}
