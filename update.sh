6ee645a2b651583eaaa318ce07b171a3041101f5
Strejda603 <strejda603@centrum.cz>
2019-08-31 13:50:37 +0200

    pkg update

--- update.sh
+++ update.sh
@@ -8,8 +8,3 @@ echo "Compressing Packages..."
 bzip2 -5fkv Packages > Packages.bz2
 xz -5fkev Packages > Packages.xz
 xz -5fkev --format=lzma Packages > Packages.lzma
-
-#chmod
-echo "Changing permissions..."
-chmod +x packageInfo/*
-chmod +x sileo-depictions/*
