69bc62a582bd39aa3bc24a28a3f24e11a6b2d214
Strejda603 <strejda603@centrum.cz>
2019-09-01 21:10:30 +0200

    Update description.js

--- description.js
+++ description.js
@@ -1,10 +1,17 @@
-function iOSVersion() {
-	var match = (navigator.appVersion).split('OS ');
-	if (match.length > 1) {
-		return match[1].split(' ')[0].split('_').join('.');
-	}
-	return false;
+function iOSversion() {
+  if (/iP(hone|od|ad)/.test(navigator.platform)) {
+    // supports iOS 2.0 and later: <http://bit.ly/TJjs1V>
+    var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
+    return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
+  }
 }
+
+ver = iOSversion();
+
+if (ver[0] >= 11) {
+  alert('This is running iOS 11 or later.');
+}
+
 
 function loadPackageInfo() {
 	var urlSplit = window.location.href.split('description.html?id=');
