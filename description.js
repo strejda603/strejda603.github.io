8d84a4e59a9f3ce1162097c9e5a647ab1e87237e
Strejda603 <strejda603@users.noreply.github.com>
2019-09-01 22:43:55 +0200

    pkg update

--- description.js
+++ description.js
@@ -1,5 +1,4 @@
-$(document).ready(function() {
-	function iOSversion() {
+function iOSversion() {
 		if (/iP(hone|od|ad)/.test(navigator.platform)) {
 			var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
 			return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
@@ -10,9 +9,7 @@ $(document).ready(function() {
 
 	if (ver[0] >= 5) {
 		alert('This is running iOS '+ver);
-		/*$('body').addClass('iOS-tagged');
-	*/}
-});
+	}
 
 
 function loadPackageInfo() {
