222557745a3f4b8ebe33b3c84111c2eb6c120064
Strejda603 <strejda603@centrum.cz>
2019-09-01 19:23:55 +0200

    Update iosver.js

--- iosver.js
+++ iosver.js
@@ -3,18 +3,17 @@ const VERSION_CHECK_SUPPORTED = "<p>iOS %s is supported. ✓</p>";
 const VERSION_CHECK_UNSUPPORTED = "<p>iOS %s is not supported. ✕</p>";
 const VERSION_CHECK_UNCONFIRMED = "<p>iOS %s has not been tested!</p>";
 
-function iOSversion() {
-	if (/iP(hone|od|ad)/.test(navigator.platform)) {
-		var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
-		return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
-	}
-	else {
-		return 0;
-	}
-}
+$(document).ready(function() {
+    function iOSversion() {
+        if (/iP(hone|od|ad)/.test(navigator.platform)) {
+            var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
+            return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
+        }
+    }
+});
 
-var maxios = 11.0;
-var minios = 12.4;
+var maxios = "11.0";
+var minios = "12.4";
 var version = iOSversion();
 var versionNumber = parseFloat(version[0] + "." + version[1]);
 
