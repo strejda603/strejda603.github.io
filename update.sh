e8f390f07cdf49fff7af412c39af7c536a416397
Strejda603 <32793325+strejda603@users.noreply.github.com>
2020-03-23 11:19:16 +0100

    pkg update

--- update.sh
+++ update.sh
@@ -8,3 +8,9 @@ echo "Compressing Packages..."
 bzip2 -5fkv Packages > Packages.bz2
 xz -5fkev Packages > Packages.xz
 xz -5fkev --format=lzma Packages > Packages.lzma
+
+echo "Setting Permissions..."
+chmod -R 644 debs/*
+chmod 700 depictions/*
+chmod 644 depictions/*/*
+chmod -R 644 sileo-depictions/*
